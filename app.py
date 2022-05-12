54aff8fc7444e3feedd38e6cf558f32bb4be4be9
SsantaJ <104478309+SsantaJ@users.noreply.github.com>
2022-05-12 17:43:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,25 @@ app = Dash(__name__)
 
 #configurar el sitio web
 app.layout = html.Div([html.H1('Programa de Samuel Santa'),
-                       html.Div('BLA BLA BLA BLA BLA BLA BLA...')])
+                       html.Div('BLA BLA BLA BLA BLA BLA BLA...'),
+                       html.Div('╔═══╗ ♪'),
+			                 html.Div('║███║ ♫'),
+                       html.Div('║ (●) ♫'),
+			                 html.Div('╚═══╝♪♪ '),
+			                 html.Div(''),
+			                 html.Div('______██████████████'),
+			                 html.Div('-____██▓▓▓▓▓▓▓▓▓ M ▓████'),
+			                 html.Div('-__██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██'),
+			                 html.Div('-__██████░░░░██░░██████'),
+			                 html.Div('██░░░░████░░██░░░░░░░░██'),
+			                 html.Div('██░░░░████░░░░██░░░░░░██'),
+			                 html.Div('   ████░░░░░░██████████'),
+			                 html.Div('-__██░░░░░░░░░░░░░██'),
+                       html.Div('_____██░░░░░░░░░██'),
+                       html.Div('-______██░░░░░░██'),
+                       html.Div('-____██▓▓████▓▓▓█'),
+                       html.H6('-------'),
+                       html.H6(':)')])
 #funcion principal
 if __name__ == '__main__':
     #cargar el objeto principal a todas las interfaces de red en el puerto 80
